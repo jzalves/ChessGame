899d1f5a8fba4ab8ae3e0bfa277caed54f0e9648
José Victor <160529147+jzalves@users.noreply.github.com>
2025-03-27 14:28:41 -0300

    Criando a classe posição
    *Pasta tabuleiro;
    *Classe posição

--- ChessGame/ChessGame/Program.cs
+++ ChessGame/ChessGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace ChessGame
 {
@@ -6,7 +7,9 @@ namespace ChessGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p;
+            p = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + p);
         }
     }
 }
