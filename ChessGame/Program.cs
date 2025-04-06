bd9b03c63c12806e813fb44f6b18b7b5e6c8be1a
José Victor <160529147+jzalves@users.noreply.github.com>
2025-04-06 10:40:28 -0300

    Erros com exception
    *Criado nova exception;
    *Aplicada na classe tabuleiro;

---
+++ ChessGame/Program.cs
@@ -0,0 +1,28 @@
+﻿using xadrez;
+using tabuleiro;
+
+namespace ChessGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+                Tela.imprimirTabuleiro(tab);
+                
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
